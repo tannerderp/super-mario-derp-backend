42da58455acc6be41aa4b953f37a0297ca017c84
Tannerderp <tannersingleton9@gmail.com>
2019-06-22 02:02:54 +0000

    posting levels

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 mongoose.connect("mongodb+srv://publicguy37:yaydatabases@cluster0-owcel.mongodb.net/community_levels?retryWrites=true&w=majority", { useNewUrlParser: true })
 var levelSchema = new mongoose.Schema({
     name: String,
-    level: [String],
+    map: [String],
     creator: String,
 });
 var Level = mongoose.model("Level", levelSchema);
@@ -25,6 +25,24 @@ app.get("/levels", function(req, res){
         }
     })
 });
+
+app.post("/newlevel", function(req, res){
+    var b = req.body;
+    var newLevel = {
+        name: b.name || "Untitled",
+        map: b.map || [""],
+        creator: b.creator || "Secret Boi",
+    }
+    Level.create(newLevel, function(error, level){
+        if(error){
+            console.log("epic error:");
+            console.log(error);
+        } else{
+            console.log("sucessful level upload!!");
+            console.log(level);
+        }
+    })
+})
 
 app.listen(process.env.PORT || 9000, process.env.IP, function(){
     console.log("Server has started on port " + (process.env.PORT || 9000));
